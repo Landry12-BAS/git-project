1b9676c564e1bcfce9f7d7018dd6c68f974fee26
Landry Bodjona <landry.myself@gmail.com>
2022-02-12 21:42:23 +0100

    3eme

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.page_view, name='pages'),
+    path('',views.page_view, name='home'),
+    path('about/', views.about_view, name='about'),
+    path('service/', views.services_view, name='services'),
+    path('contact/', views.contact_view, name='contact'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
 
 def page_view(request):
-    return render(request, 'pages/home.html')
+    return render(request, 'pages/index.html')
+
+
+def about_view(request):
+    return render(request, 'pages/about.html')
+
+
+def services_view(request):
+    return render(request, 'pages/services.html')
+
+def contact_view(request):
+    return render(request, 'pages/contact.html')
